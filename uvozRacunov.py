cb9fcf02dbcab8e261a1d49a128dfbf45c686b2e
Jernej Krum <KrumJ12@users.noreply.github.com>
2016-12-01 14:49:27 +0100

    Update uvozRacunov.py

--- uvozRacunov.py
+++ uvozRacunov.py
@@ -3,20 +3,24 @@ import sqlite3
 conn = sqlite3.connect('lokal.db')
 c = conn.cursor()
 c.execute('DELETE FROM racuni')
+c.execute('DELETE FROM nakupi')
 # vsakič izpraznemo tabelo pred novim vnosom
 
-# cene izdelkov
-c.execute('SELECT cena FROM izdelki')
+# cene, idji izdelkov
+c.execute('SELECT id,cena FROM izdelki')
 i = 0
 cene = []
-for cena in c.fetchall():
-    cene.append(cena[0])
+idji = []
+for el in c.fetchall():
+    idji.append(el[0])
+    cene.append(el[1])
 
 # seznam IDjev natakarjev
 c.execute('SELECT id FROM zaposleni WHERE funkcija = 4')
 natakarID = []
 for natakar in c.fetchall():
     natakarID.append(natakar[0])
+
 
 n = 100 # stevilo generiranih racunov
 for racun in range(n):
@@ -36,8 +40,12 @@ for racun in range(n):
     znesek = 0
     koliko = random.randint(1,3)
     for x in range(koliko):
-        količina = random.randint(1,2)
-        znesek += količina*random.choice(cene)
+        kolicina = random.randint(1,2)
+        artikel = random.randint(0,len(cene)-1)
+        znesek += kolicina*cene[artikel]
+        # vnesemo sproti v nakupe
+        stavek2 = 'INSERT INTO nakupi (id_racuna,id_izdelka,kolicina) VALUES (?,?,?)'
+        c.execute(stavek2, (st,idji[artikel],kolicina))
 
     # cas
     leto = random.choice(['2011','2012','2013','2014','2015','2016'])
@@ -55,4 +63,5 @@ for racun in range(n):
     # vnesimo v bazo
     stavek = 'INSERT INTO racuni (id,id_natakarja,znesek,cas_nakupa,nacin_placila) VALUES (?,?,?,?,?)'
     c.execute(stavek, (st,natakar,round(znesek,2),cas,nacin))
+
 conn.commit()
